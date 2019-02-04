ebb43fb31af1bfc5652d1104d534ec2c4318f129
Moath <mfalthaw@ucalgary.ca>
2019-02-04 14:00:55 +0300

    inclass code

---
+++ code/const.js
@@ -0,0 +1,8 @@
+// id
+const id = 10086650;
+// name
+let name = "Amal";
+// address
+let address = "Khobar <3";
+// age
+let age = 42;
--- code/scope.js
+++ code/scope.js
@@ -1,40 +1,104 @@
-// // A
-// var global = "A Global Variable"; // 1
+// // var global = "I'm a global variable";
 
-// function doSomething() { // 3
-//   var local = "Local Variable: Do Something";
-//   console.log(local);
-// }
+// // function notSoGlobal() {
+// //   var local = "I'm a local variable";
+// // }
 
-// console.log(global); // 2
-// doSomething();
-// console.log(local);
-// // ----------------------------------------------------------------
-// // B
-// var global = "Global Scope";
+// // console.log(global);
+// // console.log(local);
 
-// function createInnerScope() { // 1
-//   var innerScope = "Inner Scope";
+// // Create a globalVariable
+// // Create a function localFunction
+// // create a localVariable
+// // Create another localLocalFunction
+// // create a localLocalVariable
 
-//   function createInnerInnerScope() { // 2
-//     var innerInnerScope = "InnerInner Scope";
-//     // 3. What can we access from here?
+// var globalVariable = "Global Scope";
+// var localVariable = "Local but global Scope";
+
+// function localFunction() {
+//   localVariable = "Local Scope";
+//   function localLocalFunction() {
+//     var localLocalVariable = "Local Local Scope";
+//     // 1. What can I access from here?
 //   }
-
-//   createInnerInnerScope();
-//   // 2. What can we access from here?
+//   // 2. What can I access from here?
+//   localLocalFunction();
 // }
 
-// createInnerScope();
-// // 1. What can we access from here?
+// // 3. What can I access from here?
+// localFunction();
+// console.log(localVariable);
 
-// ----------------------------------------------------------------
-// C
-// console.log(someVariable);
 
-// sayHello();
-// function sayHello() {
-//   console.log("Hi there");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // // A
+// // var global = "A Global Variable"; // 1
+
+// // function doSomething() { // 3
+// //   var local = "Local Variable: Do Something";
+// //   console.log(local);
+// // }
+
+// // console.log(global); // 2
+// // doSomething();
+// // console.log(local);
+// // // ----------------------------------------------------------------
+// // // B
+// // var global = "Global Scope";
+
+// // function createInnerScope() { // 1
+// //   var innerScope = "Inner Scope";
+
+// //   function createInnerInnerScope() { // 2
+// //     var innerInnerScope = "InnerInner Scope";
+// //     // 3. What can we access from here?
+// //   }
+
+// //   createInnerInnerScope();
+// //   // 2. What can we access from here?
+// // }
+
+// // createInnerScope();
+// // // 1. What can we access from here?
+
+// // ----------------------------------------------------------------
+// // C
+// // console.log(someVariable);
+
+// // sayHello();
+// // function sayHello() {
+// //   console.log("Hi there");
+// // }
+
+// // var someVariable = true;
+
+
+
+
+// function foo() {
+//   var someVariable = "I'm random";
 // }
+console.log(someVariable);
+var someVariable = "I'm random";
 
-// var someVariable = true;
+
+// sayHi();
+// function sayHi() {
+//   console.log("Not hi");
+// }
