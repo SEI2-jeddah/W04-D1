ce5f48d014dae1d103cc710515551c8a0099e27d
Yasir S <yasser9946.1@gmail.com>
2019-10-19 23:21:26 +0300

    added more exrsice

--- laps /lap-1 /Arrow.js
+++ laps /lap-1 /Arrow.js
@@ -1,8 +1,37 @@
-// 1 - define a function By Arrow function 
+//1 -  Create a function divide between two numbers 
+
+// 2 - Only get the numbers that are divisible by 3
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+
+
+// 3 -  Create a function called fullName
+//  fullName receives two parameters, first and last and returns the full name
+//  a. Use the function keyword
+//  b. Rewrite using an arrow function
+//  c. Rewrite and use implicit return
+
+
+
+// 4 - Create an array of names (maybe use 3 of your friends)
+// Use filter to return the names with the letter "a" in them
+// Use Arrow function
+// Use implicit return
+ 
+
+
+// 5 - define a function By Arrow function 
 // Using this function to find a number that divides 1 to 10 at the same time 
 
-// 2 - define a function By Arrow function 
+
+
+// 6 - define a function By Arrow function 
 // Using this function we want to rearrange the phrase
 // phrase = "three one five four two "
 // the output most be like :
 // "one two three four five " 
+
+
+
+
