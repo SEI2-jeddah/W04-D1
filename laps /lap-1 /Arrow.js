8f60e95d8bf2873fa58e5134efa15cc572b3e163
Yasir S <yasser9946.1@gmail.com>
2019-10-19 23:12:13 +0300

    added the lap1

---
+++ laps /lap-1 /Arrow.js
@@ -0,0 +1,8 @@
+// 1 - define a function By Arrow function 
+// Using this function to find a number that divides 1 to 10 at the same time 
+
+// 2 - define a function By Arrow function 
+// Using this function we want to rearrange the phrase
+// phrase = "three one five four two "
+// the output most be like :
+// "one two three four five " 
